fe3190c0c3423fb447c045fe99e2ccc9b257803f
Keith <kandbfrank@gmail.com>
2020-11-19 15:19:27 -0500

    added {cache:no-store} to prevent responses from being cached by browser

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ async function getQuote() {
   const proxyUrl = 'http://api.allorigins.win/get?url=';
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
-    const response = await fetch(proxyUrl + encodeURIComponent(apiUrl));
+    const response = await fetch(proxyUrl + encodeURIComponent(apiUrl), {cache: 'no-store'});
     let data = await response.text();
     let quoteObjString = JSON.parse(data).contents;
     if (previousQuoteObjString === quoteObjString) {
